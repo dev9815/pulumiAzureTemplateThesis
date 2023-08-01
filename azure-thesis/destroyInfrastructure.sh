9006f9dcf01d29a1b3b912bcefbf43514797886a
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-01 16:34:35 +0200

    Update destroyInfrastructure.sh

--- azure-thesis/destroyInfrastructure.sh
+++ azure-thesis/destroyInfrastructure.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-stacks=("sync-resources" "async-resources")
-if [ -e "./Pulumi.sync-resources.yaml" ] && [ -e "./Pulumi.async-resources.yaml" ] ; then
-	for stack in "${stacks[@]}"
-	do
-		echo "Destroying Stack : $stack"
-		pulumi stack select $stack
-		pulumi destroy --yes
-		pulumi stack rm --stack $stack --yes
-	done
+if [ -e "./Pulumi.async-resources.yaml" ] ; then
+    arg=$1
+    if [ ! -n "$1" ]; then
+    	arg="dev"
+    fi
+    stacks=($arg "async-resources")
+    for stack in "${stacks[@]}"
+    do
+	echo "Destroying Stack : $stack"
+	pulumi stack select $stack
+	pulumi destroy --yes
+	pulumi stack rm --stack $stack --yes
+    done
 else
-	echo "Cannot perform destroying. One stack between ${stacks[0]} and ${stacks[1]} could not exist yet"
+    echo "Cannot perform destroying. One stack between ${stacks[0]} and ${stacks[1]} could not exist yet"
 fi
