eb24b946d71292eeac169c6ed53d8e135fcb0b28
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-01 15:41:40 +0200

    Update createInfrastructure.sh

--- azure-thesis/createInfrastructure.sh
+++ azure-thesis/createInfrastructure.sh
@@ -4,23 +4,21 @@ branches=("master" "stopDbBranch")
 functionsName=("startDB" "stopDB")
 length=${#branches[@]}
 if [ ! -d "./${functionsName[0]}" ] || [ ! -d "./${functionsName[1]}" ] ; then
-	for ((i = 0;i < length; i++ )); do
-		echo "Creating ${functionsName[$i]} azure function ..."
-		func init "${functionsName[$i]}" --worker-runtime node --language javascript
-		cd "${functionsName[$i]}"
-		echo "Installing @azure/arm-postgresql-flexible and @azure/identity SDKs ..."
-		npm install --save @azure/arm-postgresql-flexible
-		npm install --save @azure/identity
-		
-		cd ..
-	done
-	cp -r modules/startDB "${functionsName[0]}/"
-	cp -r modules/stopDB "${functionsName[1]}/"
-	echo "Azure functions ${functionsName[0]} and ${functionsName[1]} have been created successfully"
+    for ((i = 0;i < length; i++ )); do
+	echo "Creating ${functionsName[$i]} azure function ..."
+	func init "${functionsName[$i]}" --worker-runtime node --language javascript
+	cd "${functionsName[$i]}"
+	echo "Installing @azure/arm-postgresql-flexible and @azure/identity SDKs ..."
+	npm install --save @azure/arm-postgresql-flexible
+	npm install --save @azure/identity	
+	cd ..
+    done
+    cp -r modules/startDB "${functionsName[0]}/"
+    cp -r modules/stopDB "${functionsName[1]}/"
+    echo "Azure functions ${functionsName[0]} and ${functionsName[1]} have been created successfully"
 fi
 if [ ! -e "./Pulumi.async-resources.yaml" ] ; then
 	find . -maxdepth 1 -type f -name "Pulumi.*.yaml" -exec cp {} Pulumi.async-resources.yaml \;
-	pulumi stack init "${stacks[0]}"
 	pulumi stack init "${stacks[1]}"
 fi
 
