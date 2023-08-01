0084267422baf32b2d9b78c5842c3e1d9ba22cd0
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-01 15:33:39 +0200

    Update createInfrastructure.sh

--- azure-thesis/createInfrastructure.sh
+++ azure-thesis/createInfrastructure.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-stacks=("sync-resources" "async-resources")
+stacks=($1 "async-resources")
 branches=("master" "stopDbBranch")
 functionsName=("startDB" "stopDB")
 length=${#branches[@]}
@@ -11,14 +11,15 @@ if [ ! -d "./${functionsName[0]}" ] || [ ! -d "./${functionsName[1]}" ] ; then
 		echo "Installing @azure/arm-postgresql-flexible and @azure/identity SDKs ..."
 		npm install --save @azure/arm-postgresql-flexible
 		npm install --save @azure/identity
-		cp ./modules/startDB "./${functionsName[0]}/"
-		cp ./modules/stopDB "./${functionsName[1]}/"
+		
 		cd ..
 	done
-	echo "azure Functions ${functionsName[0]} and ${functionsName[1]} have been created successfully"
+	cp -r modules/startDB "${functionsName[0]}/"
+	cp -r modules/stopDB "${functionsName[1]}/"
+	echo "Azure functions ${functionsName[0]} and ${functionsName[1]} have been created successfully"
 fi
-if [ ! -e "./Pulumi.sync-resources.yaml" ] && [ ! -e "./Pulumi.async-resources.yaml" ] ; then
-	find . -maxdepth 1 -type f -name "Pulumi.*.yaml" -exec bash -c 'cp {} Pulumi.sync-resources.yaml && cp {} Pulumi.async-resources.yaml' bash \;
+if [ ! -e "./Pulumi.async-resources.yaml" ] ; then
+	find . -maxdepth 1 -type f -name "Pulumi.*.yaml" -exec cp {} Pulumi.async-resources.yaml \;
 	pulumi stack init "${stacks[0]}"
 	pulumi stack init "${stacks[1]}"
 fi
