5a49a0a1b680c844a2dc1ed6346341aca5ac39c9
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-01 16:55:22 +0200

    Update deployTemplate.sh

--- deployTemplate.sh
+++ deployTemplate.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+arg=$1
 if [ ! -d "./azure-infrastructure-from-template" ]; then
 	mkdir azure-infrastructure-from-template && cd azure-infrastructure-from-template
-	pulumi new https://github.com/dev9815/pulumiAzureTemplateThesis.git
+	if [ ! -n "$1" ]; then
+		arg="dev"
+	fi
+	pulumi new https://github.com/dev9815/pulumiAzureTemplateThesis.git --stack $arg
 	sed -i 's/\r//g' destroyInfrastructure.sh
-	sed -i 's/\r//g' createInfrastructure.sh && . ./createInfrastructure.sh
+	sed -i 's/\r//g' createInfrastructure.sh && . ./createInfrastructure.sh $arg
 else
 	echo "Delete azure-infrastructure-from-template folder and run again this script"
 fi
