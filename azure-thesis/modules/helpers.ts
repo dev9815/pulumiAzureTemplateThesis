56b40df8b1fac6857f5a4e500b94c0a695c10128
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-05 17:05:24 +0200

    Update helpers.ts

--- azure-thesis/modules/helpers.ts
+++ azure-thesis/modules/helpers.ts
@@ -1,23 +1,22 @@
 // Copyright 2016-2021, Pulumi Corporation.  All rights reserved.
-
 import * as resources from "@pulumi/azure-native/resources";
 import * as storage from "@pulumi/azure-native/storage";
 import * as pulumi from "@pulumi/pulumi";
-
 export function getConnectionString(resourceGroupName: pulumi.Input<string>, accountName: pulumi.Input<string>): pulumi.Output<string> {
     // Retrieve the primary storage account key.
     const storageAccountKeys = storage.listStorageAccountKeysOutput({ resourceGroupName, accountName });
     const primaryStorageKey = storageAccountKeys.keys[0].value;
-
     // Build the connection string to the storage account.
     return pulumi.interpolate`DefaultEndpointsProtocol=https;AccountName=${accountName};AccountKey=${primaryStorageKey}`;
 }
 
-export function signedBlobReadUrl(blob: storage.Blob,
-                                  container: storage.BlobContainer,
-                                  account: storage.StorageAccount,
-                                  resourceGroupName: pulumi.Input<string>): pulumi.Output<string> {
-
+export function signedBlobReadUrl(
+    blob: storage.Blob,
+    container: storage.BlobContainer,
+    account: storage.StorageAccount,
+    resourceGroupName: pulumi.Input<string>
+): pulumi.Output<string> 
+{
     const blobSAS = storage.listStorageAccountServiceSASOutput({
         accountName: account.name,
         protocols: storage.HttpProtocol.Https,
