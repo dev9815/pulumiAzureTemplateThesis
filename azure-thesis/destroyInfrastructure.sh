34a0f9e7bf63ee978ca9bd4de2324ccfda78813b
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-05 17:09:10 +0200

    Update destroyInfrastructure.sh

--- azure-thesis/destroyInfrastructure.sh
+++ azure-thesis/destroyInfrastructure.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-if [ -e "./Pulumi.async-resources.yaml" ] ; then
+if [ -e "./Pulumi.async-resources.yaml" ] ; then	
     arg=$1
     if [ ! -n "$1" ]; then
-  	arg="dev"
-    fi
-    stacks=($arg "async-resources")
-    for stack in "${stacks[@]}"
-    do
-	echo "Destroying Stack : $stack"
- 	pulumi stack select $stack
-	pulumi destroy --yes
-	pulumi stack rm --stack $stack --yes
-    done
+		echo "You must provide the name of the stack to operate on"
+    else
+		jsonFiles=("sync-resources.json" "async-resources.json")
+		length=${#jsonFiles[@]}
+		stacks=($arg "async-resources")
+		for ((i = 0;i < length; i++ )); 
+		do
+			echo "Destroying Stack : ${stacks[$i]}"
+			pulumi stack select "${stacks[$i]}"
+			pulumi destroy --yes
+			rm -f ./"${jsonFiles[$i]}"
+		done		
+		
+	fi
 else
     echo "Cannot perform destroying. Stacks could not exist yet"
 fi
