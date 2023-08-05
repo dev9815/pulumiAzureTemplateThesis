a22d5f4b95c61d1487aed88d40bf34b65bcd0cd5
dev9815 <80573686+dev9815@users.noreply.github.com>
2023-08-05 18:01:35 +0200

    Adding import folder
    Import folder contains json files which collect the whole infrastructure and a importInfrastructure.sh script that generates the code from the json file for importing the infrastructure. The idea is letting to use the code files in order to deploy up to different regions, simulating so the disaster recovery

---
+++ azure-thesis/import/importInfrastructure.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+arg=$1
+stacks=("sync-resources-stack" "async-resources-stack")
+folders=("sync-resources" "async-resources")
+length=${#stacks[@]}
+if [ ! -n "$1" ]; then
+	arg="azure-infrastructure-from-template"
+fi
+if [ ! -d "./${arg}" ]; then
+	#!/bin/bash
+
+	# Read the JSON data from the file and update each name using jq
+	#jq 'map(.name |= "i" + .)' async-resources.json > temp.json
+
+	# Replace the original file with the modified content
+	#mv temp.json async-resources.json
+#!/bin/bash
+
+# Use sed to update the names in the JSON file
+	#sed -i 's/"name": "\(.*\)"/"name": "i\1"/g' sync-resources.json
+	#sed -i 's/"name": "\(.*\)"/"name": "i\1"/g' async-resources.json
+	
+	
+	
+	mkdir $arg && cd $arg
+	#pulumi new azure-typescript --stack imported-resources-stack
+	mkdir sync-resources && mkdir async-resources
+	pulumi import --file ../sync-resources.json --protect=false --stack imported-resources-stack --yes --out ./sync-resources/index.ts
+	pulumi import --file ../async-resources.json --protect=false --stack imported-resources-stack --yes --out ./async-resources/index.ts
+	#location=$(grep "azure-native:location" Pulumi.imported-resources-stack.yaml | sed -n 's/^[[:space:]]*azure-native:location:[[:space:]]*//p')
+	sed -i "s/westeurope/NorthEurope/gi; s/West Europe/NorthEurope/gi" ./sync-resources/index.ts
+	sed -i "s/westeurope/NorthEurope/gi; s/West Europe/NorthEurope/gi" ./async-resources/index.ts
+	#npm install --save @pulumi/azure
+	#npm install --save @pulumi/azuread
+	#cp Pulumi.imported-resources-stack.yaml Pulumi.async-resources-stack.yaml
+	#cp Pulumi.imported-resources-stack.yaml Pulumi.sync-resources-stack.yaml
+	#rm -f index.ts
+	#for ((i = 0;i < length; i++ )); 
+	#do
+	#	pulumi stack init "${stacks[$i]}"
+	#	pulumi stack select "${stacks[$i]}"
+	#	cp "${folders[$i]}"/index.ts .
+	#	specific_string="i"
+	#	#sed -E -i "s/\b([a-zA-Z0-9_]*name[a-zA-Z0-9_]*\s*:\s*)\"([^\"]*)\"/\1\"${specific_string}\2\"/g" index.ts
+		
+		#sed -E -i "s/(\s*.*name.*:\s*\")([^\"]*)(\".*)/\1${specific_string}\2\3/g" index.ts
+	#	sed -i '/{/{:a;N;/}/!ba;s/\(\b[a-zA-Z0-9_]*name[a-zA-Z0-9_]*[[:space:]]*:[[:space:]]*"\)/\1'"${specific_string}"'/gi' index.ts
+	#	pulumi up --yes --stack "${stacks[$i]}"
+	#	rm -f index.ts
+	#done
+	#cd ../async-resources && pulumi up --yes --stack async-resources-stack
+else
+	echo "Run again this script by changing the folder name where to create the infrastructure from import"
+fi
+
